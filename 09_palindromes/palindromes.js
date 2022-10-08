5ad9d9ea34695a4a8842282b80b9335b51105c6b
Dina Rosita <dina@koudys.com>
2022-10-08 19:33:13 -0400

    Edit 09 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const new = str
+        .replace(/[^a-z]/gi, "")
+        .toLowerCase();
 
+    const l = new.length;
+    for (let i = 0; i < l/2; i++) {
+        if (new.charAt(i) !== new.charAt(l-1-i)) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if (a <= 0) {
+        return "OOPS";
+    }
+    const fib = [1, 1];
+    for (let i = 2; i < a; i++) {
+        fib.push(fib[i-1] + fib[i-2]);
+    }
+    return fib[a-1];
 };
 
 // Do not edit below this line
