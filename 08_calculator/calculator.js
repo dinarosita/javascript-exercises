f0d78fe9ee0b07661f6aeb3a49c13ed40aa12614
Dina Rosita <dina@koudys.com>
2022-10-08 18:40:17 -0400

    Do 08 11 12

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total,el) => total+el, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((total,el) => total*el, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let n = 1;
+    for (let i = 1; i <= b; i++) {
+        n *= a;
+    }
+    return n;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let n = 1;
+    for (let i = 1; i <= a; i++) {
+        n *= i;
+    }
+    return n;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let obj;
+    arr.reduce((oldest, el) => {
+        if (!el.yearOfDeath) {
+            el.yearOfDeath = new Date().getFullYear();
+        }
+        const age = el.yearOfDeath - el.yearOfBirth;
+        if (age > oldest) {
+            obj = el;
+            return age;
+        } else {
+            return oldest;
+        }
+    }, 0)
+    return obj;
 };
 
 // Do not edit below this line
